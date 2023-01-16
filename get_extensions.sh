3c1d0856dc6d9e85b53e81f195787ccbc010f9d4
flooijdt <flooijdt@localhost.localdomain>
2023-01-16 18:04:46 -0300

    default committt

--- get_extensions.sh
+++ get_extensions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-read -p "What is your first name?\n" name
-read -p "What is your surname?\n" surname
-read -N 4 -p "What is your extension number?\n" extension
-read -N 4 -p "What access code would you like to use when dialing in?\n" access
+read -p "What is your first name?: " name
+read -p "What is your surname?: " surname
+read -s -N 4 -p "What is your extension number? (must be 4 digits): " extension
+echo
+read -s -N 4 -p "What access code would you like to use when dialing in? (must be 4 digits): " access
+
+echo "$name,$surname,$extension,$access" >> extensions.csv
 
 exit 0
