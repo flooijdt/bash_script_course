de413635362dbec12fd0610141e8fe21adde1587
flooijdt <flooijdt@localhost.localdomain>
2023-01-17 16:05:10 -0300

    default committt

--- get_extensions.sh
+++ get_extensions.sh
@@ -5,7 +5,7 @@ read -p "What is your surname?: " surname
 read -s -N 4 -p "What is your extension number? (must be 4 digits): " extension
 echo
 read -s -N 4 -p "What access code would you like to use when dialing in? (must be 4 digits): " access
-
+echo
 # part two: select command
 PS3="What kind of phone do you desire? "
 select phone in headset handheld;
