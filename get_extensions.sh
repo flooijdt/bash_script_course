909d3a41f197ca38639000cf24554cee2a819ba3
flooijdt <flooijdt@localhost.localdomain>
2023-01-17 16:01:50 -0300

    default committt

--- get_extensions.sh
+++ get_extensions.sh
@@ -6,6 +6,24 @@ read -s -N 4 -p "What is your extension number? (must be 4 digits): " extension
 echo
 read -s -N 4 -p "What access code would you like to use when dialing in? (must be 4 digits): " access
 
-echo "$name,$surname,$extension,$access" >> extensions.csv
+# part two: select command
+PS3="What kind of phone do you desire? "
+select phone in headset handheld;
+do
+break
+done
+
+echo "You chose a $phone phone."
+
+
+PS3="What is your department? "
+select department in finance sales customer_service engineering;
+do
+break
+done
+
+echo "You selected the $department department."
+
+echo "$name,$surname,$extension,$access,$phone,$department" >> extensions.csv
 
 exit 0
--- select.sh
+++ select.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-
+# the PS3 variable holds the select
+# prompt:
+PS3="What is your favorite day? "
+# by default, the input is saved in
+# the RESPONSE variable. Here we
+# specify another name for the
+# variable: day.
 select day in mon tue wen thu fri;
 do
 echo "You chose: $day"
+# by default, select will loop.
+# we use break to break the loop.
 break
 done
 
