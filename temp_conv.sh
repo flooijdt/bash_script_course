9b8ad48947cda534418e7580a7cac4eb7ad63166
flooijdt <flooijdt@posteo.de>
2023-01-24 15:24:03 -0300

    default commit

---
+++ temp_conv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# we put the getopts command on a while
+# loop, because we want it to get all
+# options. Each time getops is runned, it
+# gets only the next option.
+
+while getopts "f:c:" opt; do
+# if the option given is neither 'c' nor 'f'
+# it will save a '?' in the opt variable.
+  case "$opt" in
+    c) result=$(echo "scale=2; ($OPTARG * (9/5)) + 32" | bc);;
+    f)result=$(echo "scale=2; ($OPTARG - 32) * (5/9)" | bc);;
+    \?);;
+  esac
+done
+
+echo "$result"
+
+exit 0
