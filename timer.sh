3c8ddd28839ed066c018154491f83f3f744d95a2
flooijdt <flooijdt@posteo.de>
2023-01-24 18:45:16 -0300

    default commit

--- timer.sh
+++ timer.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+total_seconds=""
+
 while getopts "m:s:" opt; do
   case "$opt" in
-    m) total_seconds="$(($OPTARG * 60))";;
-    s) total_seconds="$OPTARG";;
+    m) total_seconds=$(($total_seconds + $OPTARG * 60));;
+    s) total_seconds=$($total_seconds + $OPTARG);;
     \?) echo "invalid option.";;
   esac
-  while [ $total_seconds -ge 0 ]; do
-    echo $total_seconds
-    sleep 1s
-    total_seconds="$(($total_seconds-1))"
-    done
+done
+
+while [ $total_seconds -ge 0 ]; do
+  echo $total_seconds
+  sleep 1s
+  total_seconds="$(($total_seconds-1))"
 done
 echo "Time's up!"
