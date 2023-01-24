bb251897dddbd30d6a7a8a4beac529f98c2a4a49
flooijdt <flooijdt@posteo.de>
2023-01-24 16:26:26 -0300

    default commit

---
+++ timer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while getopts "m:s:" opt; do
+  case "$opt" in
+    m) total_seconds=$(($OPTVAR * 60));;
+    s) total_seconds=$OPTVAR;;
+    \?) echo "invalid option.";;
+  esac
+  echo $total_seconds
+  sleep 1s
+  total_seconds="$(echo $total_seconds-1 | bc)"
+done
